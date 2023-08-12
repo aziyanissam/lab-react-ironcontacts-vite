0c357b44d145c6179992fb2d74390c951a5f4277
Aziya Nissam <aziyanissam94@gmail.com>
2023-08-12 13:47:32 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,77 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import contacts from './contacts.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contact, setContact] = useState(contacts.slice(0))
+  const [random, setRandom] = useState(contacts.slice(5))
+  const [sortedPopularity, setSortedPopularity] = useState([])
+  const [sortedName, setSortedName] = useState([])
+
+  const RandomContact = () => {
+    const randomIndex = Math.floor(Math.random() * random.length)
+    const randomContact = random[randomIndex]
+    setContact((prevContacts) => [...prevContacts, randomContact]);
+    const updatedRandom = random.filter((_, index) => index !== randomIndex);
+    setRandom(updatedRandom);
+
+  }
+  const handleSortPopularity = () => {
+    const sortPopularity = [...contact].sort((a, b) => b.popularity - a.popularity)
+    setSortedPopularity(sortPopularity)
+  }
+
+  const handleSortName = () => {
+    const sortName = [...contact].sort((a, b) => a.name.localeCompare(b.name));
+    setSortedName(sortName);
+    setContact(sortName);
+  }
+
+  const handleDelete = (contactIndex) => {
+    const updatedContact = contact.filter((_, index) => index !== contactIndex);
+    setContact(updatedContact);
+    setSortedPopularity(updatedContact);
+    setSortedName(updatedContact);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='table'>
+        <div>
+          <h1>IronContacts</h1>
+        </div>
+        <div>
+          <button onClick={RandomContact}>Add Random Contact</button>
+          <button onClick={handleSortPopularity}>Sort by popularity</button>
+          <button onClick={handleSortName}>Sort by Name</button>
+        </div>
+        <div>
+          <table style={{ border: '1px solid black' }}>
+            <thead >
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+                <th>Won an Oscar</th>
+                <th>Won an Emmy</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {(sortedPopularity.length > 0 ? sortedPopularity : sortedName.length > 0 ? sortedName : contact).map((row, index) => (
+                <tr key={row.id}>
+
+                  <td><img src={row.pictureUrl} alt="" style={{ width: '200px', height: '200px' }} /></td>
+                  <td>{row.name}</td>
+                  <td>{row.popularity}</td>
+                  <td>{row.wonOscar ? '🏆' : ''}</td>
+                  <td>{row.wonEmmy ? '🏆' : ''}</td>
+                  <td><button onClick={() => handleDelete(index)}>Delete</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
